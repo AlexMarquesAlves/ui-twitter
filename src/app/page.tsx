49bae823685162b4001393ef06bd293c1066fbb9
¡Duque <atanaelleonardo@gmail.com>
2023-02-14 01:42:52 -0300

    feat: Paralelismo de fetch de dados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { User } from '@/components/User'
 
-export default function Home() {
+export default async function Home() {
+  // eslint-disable-next-line no-unused-vars
+  const [res1, res2] = await Promise.all([fetch(''), fetch('')])
   return (
     <>
       <h1>Home</h1>
