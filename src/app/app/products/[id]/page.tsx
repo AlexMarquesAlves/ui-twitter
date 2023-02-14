7e352efe8571e8802abb586727158959c91af9de
¡Duque <atanaelleonardo@gmail.com>
2023-02-14 01:09:49 -0300

    feat: Rotas dinâmicas

---
+++ src/app/app/products/[id]/page.tsx
@@ -0,0 +1,15 @@
+export type ProductProps = {
+  params: {
+    id: string
+  }
+}
+
+const Product = ({ params }: ProductProps) => {
+  return (
+    <>
+      <h1>Product: {params.id}</h1>
+    </>
+  )
+}
+
+export default Product
