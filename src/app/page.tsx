28578a274baf0327706476eb78f973241e403e78
¡Duque <atanaelleonardo@gmail.com>
2023-02-14 01:40:02 -0300

    feat: Fetch de dados nos componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
-import Link from 'next/link'
+import { User } from '@/components/User'
 
-export default async function Home() {
-  const response = await fetch(
-    'https://api.github.com/users/alexmarquesalves',
-    {
-      // next: {
-      //   revalidate: 30,
-      // },
-      cache: 'no-store',
-    },
-  )
-  const user = await response.json()
-
+export default function Home() {
   return (
     <>
       <h1>Home</h1>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-      <Link href={'/dashboard'}>Dashboard</Link>
+      {/* @ts-expect-error Async Server Component */}
+      <User />
     </>
   )
 }
