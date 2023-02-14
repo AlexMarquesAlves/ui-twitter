de077cf3deddc240c5c4425a52c6e143b4475892
¡Duque <atanaelleonardo@gmail.com>
2023-02-14 02:12:21 -0300

    feat: Roteamento no Next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { Repo } from '@/components/Repo'
 import { User } from '@/components/User'
+import { useRouter } from 'next/navigation'
 import { Suspense } from 'react'
 
 export default async function Home() {
   // eslint-disable-next-line no-unused-vars
   // const [res1, res2] = await Promise.all([fetch(''), fetch('')])
+  const route = useRouter()
+
+  // eslint-disable-next-line no-unused-vars
+  function submit() {
+    route.push('/')
+  }
 
   return (
     <>
