6ca486bad64afcb5b1bc6dd2c010a837f5fb7868
¡Duque <atanaelleonardo@gmail.com>
2023-02-15 01:41:10 -0300

    feat: added styles to Layout

---
+++ src/templates/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled, { css } from 'styled-components'
+
+export const Layout = styled.div`
+  ${({ theme }) => css`
+    margin: 0 auto;
+    max-width: 100rem;
+
+    display: grid;
+    grid-template-columns: 30rem 1fr;
+  `}
+`
