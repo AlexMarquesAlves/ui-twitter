9a778950ad18dccd077b2dc4c3a6bda253d2a947
¡Duque <atanaelleonardo@gmail.com>
2023-02-17 20:22:34 -0300

    feat: added styles to NewTweetForm

--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -100,3 +100,54 @@ export const TimeLineHeader = styled.div`
    }
   `}
 `
+
+export const NewTweetForm = styled.form`
+  ${({ theme }) => css`
+   padding: 2.4rem 2rem;
+   display: flex;
+   flex-direction: column;
+   gap: 0.8rem;
+
+   label {
+    display: flex;
+    gap: 1.2rem;
+
+    img {
+      width: 4.8rem;
+      height: 4.8rem;
+      border-radius: 999.9rem;
+    }
+
+    textarea {
+      flex: 1;
+      border: 0;
+      font-size: 2rem;
+      font-weight: 500;
+      gap: 1.2rem;
+      resize: none;
+
+      &:focus {
+        outline: none;
+      }
+      &::placeholder {
+        color: #5b7083;
+      }
+    }
+  }
+
+  button[type=submit] {
+    margin-left: auto;
+    background:  ${theme.colors.tweeterBlue};
+    border-radius: 999.9rem;
+    padding: 1.2rem 2.4rem;
+    color: ${theme.colors.white};
+    font-size: 1.6rem;
+    font-weight: 900;
+    border: 0;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+  `}
+`
