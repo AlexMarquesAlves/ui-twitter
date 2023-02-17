407da6a202c468cd3f6e3b55577282b4a26c7f29
¡Duque <atanaelleonardo@gmail.com>
2023-02-17 20:24:50 -0300

    feat: added styles to Separator

--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -151,3 +151,12 @@ export const NewTweetForm = styled.form`
   }
   `}
 `
+
+export const Separator = styled.div`
+  ${({ theme }) => css`
+    background: #f7f9fa;
+    border-top: 0.1rem solid #ebeef0;
+    border-bottom: 0.1rem solid #ebeef0;
+    height: 1.2rem;
+  `}
+`
