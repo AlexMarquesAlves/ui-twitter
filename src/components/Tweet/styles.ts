050dc5c55344437381c35d5d2245c5f85713d326
¡Duque <atanaelleonardo@gmail.com>
2023-02-18 01:25:33 -0300

    feat: added styles to Tweet wrapper

--- src/components/Tweet/styles.ts
+++ src/components/Tweet/styles.ts
@@ -2,7 +2,18 @@ import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.a`
   ${({ theme }) => css`
-    /*  */
+    padding: 2.4rem 2rem;
+    display: grid;
+    grid-template-columns: 4.8rem 1fr;
+    gap: 1.2rem;
+    border-bottom: #ebeef0;
+    text-decoration: none;
+
+    img {
+      width: 4.8rem;
+      height: 4.8rem;
+      border-radius: 999.9rem;
+    }
   `}
 `
 
@@ -21,5 +32,6 @@ export const TweetContentHeader = styled.div`
 export const TweetContentFooter = styled.div`
   ${({ theme }) => css`
      /*  */
+     }
   `}
 `
