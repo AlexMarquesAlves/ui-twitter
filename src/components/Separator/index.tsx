bb54602d6b82d49d3e7b7e785d3c5d47d8361ab8
¡Duque <atanaelleonardo@gmail.com>
2023-02-22 16:48:43 -0300

    feat: separate Separator into new component

--- src/components/Separator/index.tsx
+++ src/components/Separator/index.tsx
@@ -1,9 +1,9 @@
 import * as Styled from './styles'
 
 export type SeparatorProps = {
-  children: React.ReactNode | string
+  children?: React.ReactNode | string
 }
 
 export const Separator = (props: SeparatorProps) => {
-  return <Styled.Title>{props.children}</Styled.Title>
+  return <Styled.Wrapper>{props.children}</Styled.Wrapper>
 }
--- src/components/Separator/styles.ts
+++ src/components/Separator/styles.ts
@@ -1,3 +1,10 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Title = styled.h1``
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    background: #f7f9fa;
+    border-top: 0.1rem solid #ebeef0;
+    border-bottom: 0.1rem solid #ebeef0;
+    height: 1.2rem;
+  `}
+`
