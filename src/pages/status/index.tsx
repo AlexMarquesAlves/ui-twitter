eb1bd049fe5309d44febf26ab7baac7bfec54b61
¡Duque <atanaelleonardo@gmail.com>
2023-02-22 18:57:30 -0300

    feat: created code structure of tweet page

--- src/pages/status/index.tsx
+++ src/pages/status/index.tsx
@@ -1,4 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
+import { Header } from 'components/Header'
+import { Separator } from 'components/Separator'
+import { Tweet } from 'components/Tweet'
 import * as Styled from './styles'
+
+const answers = ['Concordo...', 'Olha, faz sentido!', 'Bem Profundo...']
 
 export type StatusProps = {
   title?: string
@@ -7,7 +13,31 @@ export type StatusProps = {
 const Status = ({ title }: StatusProps) => {
   return (
     <Styled.Wrapper>
-      <h1>Tweet</h1>
+      <Header title={'Tweet'} />
+
+      <Tweet
+        content={
+          'Sonha e serás livre de espírito... luta e serás livre na vida.'
+        }
+      />
+
+      <Separator />
+
+      <Styled.AnswerTweetForm>
+        <label htmlFor="tweet">
+          <img
+            src="https://github.com/alexmarquesalves.png"
+            alt="¡Duque profile picture"
+          />
+          <textarea placeholder="Tweet your answer" id="tweet" />
+        </label>
+
+        <button type="submit">Answer</button>
+      </Styled.AnswerTweetForm>
+
+      {answers.map((answer) => {
+        return <Tweet key={answer} content={answer} />
+      })}
     </Styled.Wrapper>
   )
 }
