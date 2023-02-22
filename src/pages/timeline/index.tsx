df639d2faec32cd42cbd224b7db2b211c6701391
¡Duque <atanaelleonardo@gmail.com>
2023-02-22 18:38:57 -0300

    feat: created timeline route page as / route

---
+++ src/pages/timeline/index.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable @next/next/no-img-element */
+import { Header } from 'components/Header'
+import { Separator } from 'components/Separator'
+import { Tweet } from 'components/Tweet'
+import * as Styled from './styles'
+
+const tweets = [
+  'Sonha e serás livre de espírito... luta e serás livre na vida.',
+  'O futuro pertence àqueles que acreditam na beleza de seus sonhos.',
+  'A arte é a autoexpressão lutando para ser absoluta.',
+]
+
+export type TimelineProps = {
+  children?: React.ReactNode | string
+}
+
+const Timeline = (props: TimelineProps) => {
+  return (
+    <Styled.Wrapper>
+      <Header title={'Home'} />
+
+      <Styled.NewTweetForm>
+        <label htmlFor="tweet">
+          <img
+            src="https://github.com/alexmarquesalves.png"
+            alt="¡Duque profile picture"
+          />
+          <textarea placeholder="What's happening?" id="tweet" />
+        </label>
+
+        <button type="submit">Tweet</button>
+      </Styled.NewTweetForm>
+
+      <Separator />
+
+      {tweets.map((tweet) => {
+        return <Tweet key={tweet} content={tweet} />
+      })}
+
+    </Styled.Wrapper>
+  )
+}
+
+export default Timeline
---
+++ src/pages/timeline/styles.ts
@@ -0,0 +1,58 @@
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.main`
+  ${({ theme }) => css`
+   /*  */
+  `}
+`
+
+export const NewTweetForm = styled.form`
+  ${({ theme }) => css`
+   padding: 2.4rem 2rem;
+   display: flex;
+   flex-direction: column;
+   gap: 0.8rem;
+
+   label {
+    display: flex;
+    gap: 1.2rem;
+
+    img {
+      width: 4.8rem;
+      height: 4.8rem;
+      border-radius: 999.9rem;
+    }
+
+    textarea {
+      flex: 1;
+      border: 0;
+      font-size: 2rem;
+      font-weight: 500;
+      gap: 1.2rem;
+      resize: none;
+
+      &:focus {
+        outline: none;
+      }
+      &::placeholder {
+        color: #5b7083;
+      }
+    }
+  }
+
+  button[type=submit] {
+    margin-left: auto;
+    background:  ${theme.colors.tweeterBlue};
+    border-radius: 999.9rem;
+    padding: 1.2rem 2.4rem;
+    color: ${theme.colors.white};
+    font-size: 1.6rem;
+    font-weight: 900;
+    border: 0;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+  `}
+`
